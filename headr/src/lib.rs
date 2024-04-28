0d8203f7015dcc14f9e902b0b0758db74bae1de6
Komuro Hiraku <komuro.hiraku@gmail.com>
2024-04-28 16:26:59 +0000

    - command optionを途中まで追加

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,6 +1,5 @@
 use std::error::Error;
-
-use clap::App;
+use clap::{App, Arg};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -16,7 +15,19 @@ pub fn get_args() -> MyResult<Config> {
     .version("0.1.0")
     .author("komuro-hiraku <komuro.hiraku@gmail.com>")
     .about("Rust head")
+
+    // Flag: -h, -V
+    // Options: -c, -n
+    // Args: FILE...
+    .arg(
+        Arg::with_name("number_of_bytes")
+    )
+    .arg(
+        Arg::with_name("number_of_lines")
+    )
     .get_matches();
+
+    // TODO: lines, bytes をそれぞれ数値化するとか色々
 
     Ok(Config {
         files: todo!(),
@@ -31,5 +42,28 @@ pub fn run(config: Config) -> MyResult<()> {
 }
 
 fn parse_positive_int(val: &str) -> MyResult<usize> {
-    unimplemented!();
+    match val.parse() {
+        Ok(n) if n > 0 => Ok(n),
+
+        // Err(val.into()), Err(Into::into(val)) でも良いらしい
+        _ => Err(From::from(val)),
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    // 3は正の整数なのでOK
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    // 数字でない文字列の場合はエラー
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    // 0の場合もエラー
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
 }
