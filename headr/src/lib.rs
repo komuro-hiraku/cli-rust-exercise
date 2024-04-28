395b900af74744533e41278d6a3ca8dd06f65b56
Komuro Hiraku <komuro.hiraku@gmail.com>
2024-04-28 15:47:54 +0000

    Add headr

---
+++ headr/src/lib.rs
@@ -0,0 +1,32 @@
+use std::error::Error;
+
+use clap::App;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("headr")
+    .version("0.1.0")
+    .author("komuro-hiraku <komuro.hiraku@gmail.com>")
+    .about("Rust head")
+    .get_matches();
+
+    Ok(Config {
+        files: todo!(),
+        lines: todo!(),
+        bytes: todo!(),
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
+
